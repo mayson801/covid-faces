88e83ef6bcfe62e7544d3924fa935178752f5080
mayson801 <48494078+mayson801@users.noreply.github.com>
2020-11-28 22:46:40 +0000

    testing

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 
 app = Flask(__name__)
+@app.route("/")
 def pass_to_index():
     total_count = get_total_faces()
     faces=get_faces(total_count)
