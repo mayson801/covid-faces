192693c385454a01d2518fafd04c1936d443a2b6
mayson801 <48494078+mayson801@users.noreply.github.com>
2020-11-29 22:45:32 +0000

    test

--- serving_static/serve.py
+++ serving_static/serve.py
@@ -10,7 +10,8 @@ def pass_to_index():
     return render_template('index.html', faces=faces, message=str(total_count))
 def get_total_faces():
     plz=(os.path.dirname(os.getcwd()))
-    img_folder_path = plz + '\static\people'
+    print(plz)
+    img_folder_path = plz + 'serving_static/static/people'
     dirListing = os.listdir(img_folder_path)
     return dirListing
 def get_faces(covidcases):
