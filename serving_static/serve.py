1a3d8ab0ff5aec83a221337c206209650b0b964a
mayson801 <48494078+mayson801@users.noreply.github.com>
2020-11-29 23:31:52 +0000

    test

--- serving_static/serve.py
+++ serving_static/serve.py
@@ -29,7 +29,7 @@ def get_faces(covidcases):
     return faces
 def count_index():
     plz=(os.getcwd())
-    img_folder_path = plz + '/serving_static'
+    img_folder_path = plz + '/serving_static/static'
     dirListing = os.listdir(img_folder_path)
     return dirListing
 if __name__ == "__main__":
