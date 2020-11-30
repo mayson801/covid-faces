c3fcea3c0ea935cc3e094341ddc86e2b29a7483d
mayson801 <48494078+mayson801@users.noreply.github.com>
2020-11-30 16:35:23 +0000

    update

--- autogit.py
+++ autogit.py
@@ -1,6 +1,7 @@
 import os
-os.system("python get_images.py")
-os.system("git add --all")
-os.system("git commit -m 'test'")
-os.system("git push")
-
+#os.system("python get_images.py")
+#os.system("git add .")
+#os.system("git commit -m 'test'")
+#os.system("git push")
+os.system()
+os.system("ls")
